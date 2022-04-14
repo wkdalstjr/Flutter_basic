b580231e1567f2786feddeea9b427def4fb919cc
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-14 16:28:27 +0900

    practice#4

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -15,19 +15,28 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('앱임'),
           centerTitle: false,
+          leading: Icon(Icons.star),
+          actions: [Icon(Icons.person), Icon(Icons.person)],
         ),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 80,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ]
-            ),
+        // body: Align(
+        //   alignment: Alignment.bottomCenter,
+        //   child: Container(
+        //     width: double.infinity, height: 50, color: Colors.blue,
+        //     // margin: EdgeInsets.fromLTRB(0,30,0,0),
+        //     // decoration: BoxDecoration( // 잡다한 기능은 decoration 안에다가 작성
+        //     //   border: Border.all(color: Colors.black),
+        //     //
+        //     // ),
+        //   ),
+        // ),
+        body: SizedBox(
+          child:
+          // Text('안녕하세요',
+          //   style: TextStyle(color: Colors.red, fontSize: 30, fontWeight: FontWeight.w700),
+          // ),
+          ElevatedButton(
+            child: Text('글자'),
+            onPressed: (){},
           ),
         ),
       )
