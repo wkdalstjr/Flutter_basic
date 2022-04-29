5ffb2a132fef87e42f7a5c28930fecbe332f77c4
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-29 16:17:32 +0900

    TextField & AppBar

--- memo_app/lib/screens/edit.dart
+++ memo_app/lib/screens/edit.dart
@@ -1,9 +1,51 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class EditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              //obscureText: true,
+              decoration: InputDecoration(
+                //border: OutlineInputBorder(),
+                hintText: '메모의 제목을 적어주세요',
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(10)),
+            TextField(
+              //obscureText: true,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                //border: OutlineInputBorder(),
+                hintText: '메모의 내용을 적어주세요',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
