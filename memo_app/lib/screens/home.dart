4a74d2304eadcdeb4ba202e22506a91a2c1faeff
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-29 15:48:32 +0900

    navigator push

---
+++ memo_app/lib/screens/home.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:memo_app/screens/edit.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          Navigator.push(
+            context,
+            CupertinoPageRoute(builder: (context) => EditPage())
+          );
+        },
+        tooltip: '노트를 추가하려면 클릭하세요',
+        label: Text('메모 추가'),
+        icon: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
