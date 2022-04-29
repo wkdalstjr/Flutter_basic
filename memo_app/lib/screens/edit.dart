a550252dcf7cabb266a0076f8ab3c48263bccb17
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-29 21:19:22 +0900

    sqlite basic

--- memo_app/lib/screens/edit.dart
+++ memo_app/lib/screens/edit.dart
@@ -1,10 +1,14 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, override_on_non_overriding_member, unnecessary_this
 import 'package:flutter/material.dart';
+import 'package:memo_app/database/memo.dart';
+import 'package:memo_app/main.dart';
+import 'package:memo_app/database/db.dart';
 
 class EditPage extends StatelessWidget {
 
   @override
+  String title = '';
+  String text = '';
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -15,7 +19,7 @@ class EditPage extends StatelessWidget {
           ),
           IconButton(
             icon: const Icon(Icons.save),
-            onPressed: (){},
+            onPressed: saveDB,
           )
         ],
       ),
@@ -24,6 +28,7 @@ class EditPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             TextField(
+              onChanged: (String title){ this.title = title;},
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
               keyboardType: TextInputType.multiline,
               maxLines: null,
@@ -35,6 +40,7 @@ class EditPage extends StatelessWidget {
             ),
             Padding(padding: const EdgeInsets.all(10)),
             TextField(
+              onChanged: (String text){ this.text = text;},
               //obscureText: true,
               keyboardType: TextInputType.multiline,
               maxLines: null,
@@ -47,5 +53,21 @@ class EditPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> saveDB() async {
+    DBHelper sd = DBHelper();
+
+    var fido = Memo(
+      id: 4,
+      title: this.title,
+      text: this.text,
+      createTime: DateTime.now().toString(),
+      editTime: DateTime.now().toString(),
+    );
+
+    await sd.insertMemo(fido);
+
+    print(await sd.memos());
   }
 }
