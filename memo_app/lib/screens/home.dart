4c6c2d0bbd216803990988527911bd24affee494
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-30 22:03:42 +0900

    BoxDecoration

--- memo_app/lib/screens/home.dart
+++ memo_app/lib/screens/home.dart
@@ -21,10 +21,12 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-            child: Text(
-              '메모메모',
-              style: TextStyle(fontSize: 36, color: Colors.blue),
+            padding: EdgeInsets.only(left: 5, top: 50, bottom: 20),
+            child: Container(
+              child: Text(
+                '메모메모',
+                style: TextStyle(fontSize: 36, color: Colors.blue),
+              ),
             ),
           ),
           Expanded(child: memoBuilder()),
@@ -61,19 +63,65 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context, projectSnap) {
         if ((projectSnap.data as List).isEmpty) {
           //print('project snapshot data is: ${projectSnap.data}');
-          return Container(child: Text("메모를 지금 바로 추가해보세요!"));
+          return Container(
+            alignment: Alignment.center,
+            child: Text(
+              '지금 바로 "메모 추가" 버튼을 눌러\n새로운 메모를 추가해보세요!\n\n\n\n\n\n\n\n\n',
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+          );
         }
         return ListView.builder(
+          physics: BouncingScrollPhysics(),
           itemCount: (projectSnap.data as List).length,
           itemBuilder: (context, index) {
-            Memo memo  = (projectSnap.data as List)[index];
-            return Column(
-              children: <Widget>[
-                Text(memo.title),
-                Text(memo.text),
-                Text(memo.editTime),
-                // Widget to display the list of project
-              ],
+            Memo memo = (projectSnap.data as List)[index];
+            return Container(
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 1,
+                  ),
+                  boxShadow: [BoxShadow(color: Colors.lightBlue, blurRadius: 2)],
+                  borderRadius: BorderRadius.circular(12)),
+              alignment: Alignment.center,
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        memo.title,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        memo.text,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "최종 수정 시간: " + memo.editTime.split('.')[0],
+                        style: TextStyle(fontSize: 11),
+                        textAlign: TextAlign.end,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             );
           },
         );
