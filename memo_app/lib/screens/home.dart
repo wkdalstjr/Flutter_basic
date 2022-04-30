29bd5170a3a13ceb26694965f1eaab83ad7bb208
wkdalstjr <wkdalstjr1218@naver.com>
2022-04-30 16:22:48 +0900

    FutureBuilder & ListView

--- memo_app/lib/screens/home.dart
+++ memo_app/lib/screens/home.dart
@@ -1,8 +1,10 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, non_constant_identifier_names
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, non_constant_identifier_names, unnecessary_null_comparison, avoid_unnecessary_containers
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memo_app/screens/edit.dart';
+import 'package:memo_app/database/db.dart';
+import 'package:memo_app/database/memo.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -16,26 +18,22 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        physics: BouncingScrollPhysics(),
+      body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                child: Text('메모메모',style: TextStyle(fontSize: 36, color: Colors.blue),),
-              )
-            ],
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+            child: Text(
+              '메모메모',
+              style: TextStyle(fontSize: 36, color: Colors.blue),
+            ),
           ),
-          ...LoadMemo()
+          Expanded(child: memoBuilder()),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(
-            context,
-            CupertinoPageRoute(builder: (context) => EditPage())
-          );
+              context, CupertinoPageRoute(builder: (context) => EditPage()));
         },
         tooltip: '노트를 추가하려면 클릭하세요',
         label: Text('메모 추가'),
@@ -46,7 +44,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Widget> LoadMemo() {
     List<Widget> memoList = [];
-    memoList.add(Container(color: Colors.green, height: 100,));
+    memoList.add(Container(
+      color: Colors.green,
+      height: 100,
+    ));
     return memoList;
+  }
+
+  Future<List<Memo>> loadMemo() async {
+    DBHelper sd = DBHelper();
+    return await sd.memos();
+  }
+
+  Widget memoBuilder() {
+    return FutureBuilder(
+      builder: (context, projectSnap) {
+        if ((projectSnap.data as List).isEmpty) {
+          //print('project snapshot data is: ${projectSnap.data}');
+          return Container(child: Text("메모를 지금 바로 추가해보세요!"));
+        }
+        return ListView.builder(
+          itemCount: (projectSnap.data as List).length,
+          itemBuilder: (context, index) {
+            Memo memo  = (projectSnap.data as List)[index];
+            return Column(
+              children: <Widget>[
+                Text(memo.title),
+                Text(memo.text),
+                Text(memo.editTime),
+                // Widget to display the list of project
+              ],
+            );
+          },
+        );
+      },
+      future: loadMemo(),
+    );
   }
 }
